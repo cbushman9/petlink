c2cc0bfc65fcf2bc4e5b771de5cb2e22e36cdecf
Divya <divya.arigay@gmail.com>
2022-03-22 17:05:11 -0600

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,24 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+// create connection to our database, pass in your MySQL information for username and password
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
 
 module.exports = sequelize;
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3001,
+//         dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+// module.exports = sequelize;
