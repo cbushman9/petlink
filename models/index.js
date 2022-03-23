daf910c54e99455c4a0d6762c55227f1df43b707
Anastasia Perez <Stasiperez85@gmail.com>
2022-03-22 21:49:05 -0600

    add like model and assocations

--- models/index.js
+++ models/index.js
@@ -1,13 +1,42 @@
 const User = require('./User');
 const Post = require('./Post');
+const Like = require('./Like');
 
 // // create associations
 // User.hasMany(Post, {
-//     foreignKey: 'user_id'
-//   });
+//   foreignKey: 'user_id'
+// });
 
 // Post.belongsTo(User, {
-//     foreignKey: 'user_id',
-//   });
+//   foreignKey: 'user_id',
+// });
 
-module.exports = { User, Post };
+// User.belongsToMany(Post, {
+//   through: Like,
+//   as: 'liked_posts',
+//   foreignKey: 'user_id'
+// });
+
+// Post.belongsToMany(User, {
+//   through: Like,
+//   as: 'liked_posts',
+//   foreignKey: 'post_id'
+// });
+
+// Like.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
+
+// Like.belongsTo(Post, {
+//   foreignKey: 'post_id'
+// });
+
+// User.hasMany(Like, {
+//   foreignKey: 'user_id'
+// });
+
+// Post.hasMany(Like, {
+//   foreignKey: 'post_id'
+// });
+
+module.exports = { User, Post, Like };
